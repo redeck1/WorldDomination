0b3767fd40952db5a499f4f4e94d20ea36a8aa46
LordReaps <BovenkoAndrey@gmail.com>
2024-07-26 23:44:46 +0300

    ровняю, ничего серьезного

--- backend/logic.py
+++ backend/logic.py
@@ -5,4 +5,38 @@ with open('all_state_round1.json', 'r', encoding='utf-8') as file:
     all_state = json.load(file)
     file.close()
 
-print(all_state)
+
+
+
+with open('Russia_state_changes.json', 'r', encoding='utf-8') as file:
+    Russia_changes = json.load(file)
+    file.close()
+
+with open('USA_state_changes.json', 'r', encoding='utf-8') as file:
+    USA_changes = json.load(file)
+    file.close()
+
+with open('North_Korea_state_changes.json', 'r', encoding='utf-8') as file:
+    North_Korea_changes = json.load(file)
+    file.close()
+
+with open('Iraq_state_changes.json', 'r', encoding='utf-8') as file:
+    Iraq_changes = json.load(file)
+    file.close()
+
+with open('China_state_changes.json', 'r', encoding='utf-8') as file:
+    China_changes = json.load(file)
+    file.close()
+
+with open('Greece_state_changes.json', 'r', encoding='utf-8') as file:
+    Greece_changes = json.load(file)
+    file.close()
+
+with open('France_state_changes.json', 'r', encoding='utf-8') as file:
+    France_changes = json.load(file)
+    file.close()
+
+with open('Germany_state_changes.json', 'r', encoding='utf-8') as file:
+    Germany_changes = json.load(file)
+    file.close()
+
